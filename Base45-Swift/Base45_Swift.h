f82025fb3dfb21052af0fbd4dafcedfb2b538fef
Dirk-Willem van Gulik <dirkx@webweaving.org>
2021-04-01 17:32:37 +0200

    Initial Commit

---
+++ Base45-Swift/Base45_Swift.h
@@ -0,0 +1,18 @@
+//
+//  Base45_Swift.h
+//  Base45-Swift
+//
+//  Created by Dirk-Willem van Gulik on 01/04/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Base45_Swift.
+FOUNDATION_EXPORT double Base45_SwiftVersionNumber;
+
+//! Project version string for Base45_Swift.
+FOUNDATION_EXPORT const unsigned char Base45_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Base45_Swift/PublicHeader.h>
+
+
