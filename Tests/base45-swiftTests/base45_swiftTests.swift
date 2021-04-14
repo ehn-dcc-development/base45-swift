7c5d3e0640c7f0b949bba1905306867d06f2dbb3
Dominik Mocher <dominik.mocher@a-sit.at>
2021-04-14 13:20:26 +0200

    Restructure code for distribution as swift package

---
+++ Tests/base45-swiftTests/base45_swiftTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import base45_swift
+
+    final class base45_swiftTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(base45_swift().text, "Hello, World!")
+        }
+    }
