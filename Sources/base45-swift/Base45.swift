42ce0aaef399a0ea6b423d6eadf0ef830e5a4a3f
Dirk-Willem van Gulik <dirkx@webweaving.org>
2021-05-27 18:07:31 +0200

    Fix license, remove unneeded files.

--- Sources/base45-swift/Base45.swift
+++ Sources/base45-swift/Base45.swift
@@ -2,7 +2,19 @@
 //  Base45.swift
 //  Base45-Swift
 //
-//  Created by Dirk-Willem van Gulik on 01/04/2021.
+// Copyright 2021 Dirk-Willem van Gulik, Ministry of Public Health, the Netherlands.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
 
 import Foundation
